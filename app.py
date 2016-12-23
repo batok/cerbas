e966d0e1cd88b4bbb2ba652b38fd7966eaabbf21
Domingo <domingo.aguilera@gmail.com>
2016-12-23 12:12:30 -0600

    app.py which is the flask example now get its port from cerbas

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask
+import cerbas
+import json
+from datetime import datetime
 app = Flask(__name__)
+
+
+def cerbas_port():
+    prod, local = False, True
+    cerbas.start(prod, local)
+    source = "cerbastest"
+    req = json.loads(cerbas.request(func="proxiedhostport", user="foo",
+        source=source, arguments=dict(server="flask")))
+    return req.get("data", 0)
 
 @app.route("/api5/foo")
 def hello():
-  return "foo from flask"
+    return "foo from flask at {}".format(datetime.now().isoformat())
 
 if __name__ == "__main__":
-  app.run()
+    port = cerbas_port()
+    if port:
+        app.run(host=None, port=port)
+    else:
+        print("port not available")
